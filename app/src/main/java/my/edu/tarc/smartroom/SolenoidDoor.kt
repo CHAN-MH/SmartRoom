511f8efc53f0f04899cd27a1ac080ad65e849d3f
Winnie Yap <winnieyap20@gmail.com>
2020-11-22 14:45:22 +0800

    error version to be debug ltr

--- app/src/main/java/my/edu/tarc/smartroom/SolenoidDoor.kt
+++ app/src/main/java/my/edu/tarc/smartroom/SolenoidDoor.kt
@@ -12,54 +12,52 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-lateinit var code:String
+lateinit var codePIN: String
 class SolenoidDoor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solenoid_door)
 
-        //passing the user choice through activity
-        var selection: String? = intent.getStringExtra("selection")
-
         //Accessing UI
         val psw: EditText = findViewById(R.id.password)
         val buttonProceed: Button = findViewById(R.id.buttonProceed)
+        val textView:TextView = findViewById(R.id.textView)
+
+        //passing the user choice through activity
+        var selection: String? = intent.getStringExtra("selection")
+
+        //check user selection and read from database
+        //initialize user selection as ntg
+        var roomNoSelected:String ?=null //door open
+        if (selection == "1") {
+            roomNoSelected == "Room1"
+        }
+        else if (selection == "2") {
+            roomNoSelected == "Room2"
+        }
+        else if (selection == "3") {
+            roomNoSelected == "Room3"
+        }
+        else if (selection == "4") {
+            roomNoSelected == "Room4"
+        }
 
         //accessing the database
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("Room")
+        val myRef = database.getReference("Room").child(roomNoSelected.toString()).child("code")
 
         //retrieving the pass code from firebase
-        myRef.addValueEventListener(object : ValueEventListener {
+        myRef?.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                //check for retrieving which room
-                when (selection) {
-                    "1" -> {
-                        //storing the correct password into code variable
-                        code = dataSnapshot.child("Room1").child("code").value.toString();
-                    }
-                    "2" -> {
-                        //storing the correct password into code variable
-                        code = dataSnapshot.child("Room2").child("code").value.toString();
-                    }
-                    "3" -> {
-                        //storing the correct password into code variable
-                        code = dataSnapshot.child("Room3").child("code").value.toString();
-                    }
-                    "4" -> {
-                        //storing the correct password into code variable
-                        code = dataSnapshot.child("Room4").child("code").value.toString();
-                    }
-                }
+                codePIN = dataSnapshot.value.toString();
             }
 
             override fun onCancelled(error: DatabaseError) {
-                val textView:TextView = findViewById(R.id.textView)
-                textView.text = "Sign In Function Failed"
-
+                textView.text = "Read Failed. Have a nice day"
             }
         })
 
+        //checking psw
         buttonProceed.setOnClickListener {
             //assigning user input to a variable
             val password:String = psw.text.toString().trim();
@@ -74,11 +72,8 @@ class SolenoidDoor : AppCompatActivity() {
             }
             else{
                 //do password checking
-                if(password == code){
-                    //door open
-                    Toast.makeText(baseContext, "Door Unlocked!", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, Timer::class.java)
-                    startActivity(intent)
+                if(password == codePIN){
+                    openTheDoor()
                 }
                 else{
                     psw.error = "Password does not match!";
@@ -88,4 +83,13 @@ class SolenoidDoor : AppCompatActivity() {
             }}
 
     }
+
+    private fun openTheDoor() {
+        //door open
+        Toast.makeText(baseContext, "Door Unlocked!", Toast.LENGTH_SHORT).show()
+        Thread.sleep(5_000)
+        startActivity(Intent(this, Timer::class.java))
+    }
+
+
 }
