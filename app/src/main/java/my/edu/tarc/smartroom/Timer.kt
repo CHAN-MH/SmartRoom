1a700bec716862d9a7bbbda144349119b07d3755
CHAN-MH <chanmh-wp18@student.tarc.edu.my>
2020-11-22 15:23:48 +0800

    Timer text update

--- app/src/main/java/my/edu/tarc/smartroom/Timer.kt
+++ app/src/main/java/my/edu/tarc/smartroom/Timer.kt
@@ -1,18 +1,16 @@
 package my.edu.tarc.smartroom
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import my.edu.tarc.smartroom.R
 
 class Timer : AppCompatActivity() {
-    //Global variables
-    //var START_MILLI_SECONDS = 60000L
-    //lateinit var countdown_timer: CountDownTimer
-    //var isRunning: Boolean = false;
-    //var time_in_milli_seconds = 0L
-    var time = 30
+    //Global variable
+    //var time = 50
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,16 +21,49 @@ class Timer : AppCompatActivity() {
 
         object : CountDownTimer(30000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                UItimer.setText("Time remaining: " + millisUntilFinished / 1000 + "sec")
+                UItimer.setText("seconds remaining: " + millisUntilFinished / 1000)
+                var timeleft = (millisUntilFinished / 1000).toString()
+                if (timeleft == "20")
+                {
+                    displayExtensionDialog()
+                }
             }
-
             override fun onFinish() {
-                UItimer.setText("Session End")
+                UItimer.setText("Session Ended!")
             }
         }.start()
 
-    }  //end of onCreate
-} //end of class
+    }//end of onCreate
+
+    private fun displayExtensionDialog() {
+        // create an alert builder
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Extend Session")
+
+        // set the custom layout
+        val customLayout: View = layoutInflater.inflate(R.layout.dialog_extension, null);
+        builder.setView(customLayout);
+        builder.setMessage("Session about to end. Do you want to extend your session?")
+
+        builder.apply {
+            setPositiveButton(R.string.extend, DialogInterface.OnClickListener { dialog, _ ->
+                //When user click extend
+                //textViewExtension.text = "Succesfully extend session for another 30 minute"
+                dialog.cancel()
+            })
+            setNegativeButton(R.string.nothx, DialogInterface.OnClickListener { dialog, _->
+                //When user click No, thanks
+                //.text = "Please evacuate the room before your session ends."
+                dialog.cancel()
+            })
+        }
+
+        // create and show the alert dialog
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }//end of dialog
+
+}//end of class
 
 /*
     //---------------------------------------------------------------
