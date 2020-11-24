b7b1f7dbadfbea814f2d713a9264ed2a45db61a2
CHAN-MH <chanmh-wp18@student.tarc.edu.my>
2020-11-24 21:53:30 +0800

    LCD display settle

--- app/src/main/java/my/edu/tarc/smartroom/Timer.kt
+++ app/src/main/java/my/edu/tarc/smartroom/Timer.kt
@@ -19,14 +19,19 @@ import com.google.firebase.database.*
 class Timer : AppCompatActivity() {
 
     //Global variable
-    private lateinit var selection : String
+    private lateinit var selection: String
+
     //database1 = common resources firebase
     val database1 = FirebaseDatabase.getInstance("https://bait2123-202010-03.firebaseio.com/")
+
     //primary firebase : our firebase
-    val database2: FirebaseDatabase = FirebaseDatabase.getInstance("https://solenoid-lock-f65e8.firebaseio.com/")
+    val database2: FirebaseDatabase =
+        FirebaseDatabase.getInstance("https://solenoid-lock-f65e8.firebaseio.com/")
     val timeRef = database2.getReference("Room")
+
     //Write to common resources firebase
     val data1 = database1.getReference("PI_03_CONTROL")
+
     //Write to personal firebase
     val data2 = database2.getReference("PI_03_CONTROL")
 
@@ -42,8 +47,8 @@ class Timer : AppCompatActivity() {
         timeRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 selection = dataSnapshot.child("selection").value.toString()
-
             }
+
             override fun onCancelled(error: DatabaseError) {
                 //Actions when failed to read data
                 UItimer.text = "Read failed."
@@ -58,7 +63,7 @@ class Timer : AppCompatActivity() {
                 UItimer.setText("RemainingTime : " + millisUntilFinished / 1000)
                 //var timeleft = millisUntilFinished / 1000
                 var notiTime = (millisUntilFinished / 1000).toString()
-                if (notiTime== "10") {
+                if (notiTime == "10") {
                     createNotificationChannel()
                 }
             }//end of onTick
@@ -80,60 +85,59 @@ class Timer : AppCompatActivity() {
                     }
                 }
 
-                var lcdscr = "1"
-                var lcdtxt = "****AVAILABLE****"
-                var lcdbkR = "0"
-                var lcdbkG = "20"
-                var lcdbkB = "0"
+            var lcdscr = "1"
+            var lcdtxt = "****AVAILABLE****"
+            var lcdbkR = "0"
+            var lcdbkG = "20"
+            var lcdbkB = "0"
 
-                //setting the value at common resources
-                data1.child("lcdscr").setValue(lcdscr)
-                data1.child("lcdtxt").setValue(lcdtxt)
-                data1.child("lcdbkR").setValue(lcdbkR)
-                data1.child("lcdbkG").setValue(lcdbkG)
-                data1.child("lcdbkB").setValue(lcdbkB)
+            //setting the value at common resources
+            data1.child("lcdscr").setValue(lcdscr)
+            data1.child("lcdtxt").setValue(lcdtxt)
+            data1.child("lcdbkR").setValue(lcdbkR)
+            data1.child("lcdbkG").setValue(lcdbkG)
+            data1.child("lcdbkB").setValue(lcdbkB)
 
-                //for testing purpose
-                //setting the value at personal database
-                data2.child("lcdscr").setValue(lcdscr)
-                data2.child("lcdtxt").setValue(lcdtxt)
-                data2.child("lcdbkR").setValue(lcdbkR)
-                data2.child("lcdbkG").setValue(lcdbkG)
-                data2.child("lcdbkB").setValue(lcdbkB)
-            }//end of onFinish
-        }.start()//end of timer
+            //for testing purpose
+            //setting the value at personal database
+            data2.child("lcdscr").setValue(lcdscr)
+            data2.child("lcdtxt").setValue(lcdtxt)
+            data2.child("lcdbkR").setValue(lcdbkR)
+            data2.child("lcdbkG").setValue(lcdbkG)
+            data2.child("lcdbkB").setValue(lcdbkB)
+        }//end of onFinish
+    }.start()//end of timer
 
+}//end of onCreate
 
-    }//end of onCreate
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.mychannel)
-            val descriptionText = getString(R.string.channeldesc)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("1234", name, importance).apply {
-                description = descriptionText
-            }
-            //Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+@RequiresApi(Build.VERSION_CODES.O)
+private fun createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = getString(R.string.mychannel)
+        val descriptionText = getString(R.string.channeldesc)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("1234", name, importance).apply {
+            description = descriptionText
         }
-        //Send notification to user when session about to end
-        val builder = NotificationCompat.Builder(this, "1234")
-            .setSmallIcon(R.drawable.logo)
-            .setContentTitle("Session Status")
-            .setContentText("Session about to end")
-            .setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText("Session ending in 20 minutes. Kindly evacuate the room on time.")
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        val notificationId = 5678
-        with(NotificationManagerCompat.from(this)) {
-            notify(notificationId, builder.build())
-        }
+        //Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
+    //Send notification to user when session about to end
+    val builder = NotificationCompat.Builder(this, "1234")
+        .setSmallIcon(R.drawable.logo)
+        .setContentTitle("Session Status")
+        .setContentText("Session about to end")
+        .setStyle(
+            NotificationCompat.BigTextStyle()
+                .bigText("Session ending in 20 minutes. Kindly evacuate the room on time.")
+        )
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+    val notificationId = 5678
+    with(NotificationManagerCompat.from(this)) {
+        notify(notificationId, builder.build())
+    }
+}
 
 }//end of class
